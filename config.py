35fdaad4972d654eb153ecd982ef23a09696e3d0
Faisal Fida <69955157+faisal-fida@users.noreply.github.com>
2025-05-29 13:50:42 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 CIN7_API_URL = "https://inventory.dearsystems.com/ExternalApi/v2/"
-CIN7_ACCOUNT_ID = "12999e8d-0e97-4a5d-b345-acf0f4155813"
-CIN7_APPLICATION_KEY = "57a82013-9afa-8edf-4051-6d1a44b4113c"
+CIN7_ACCOUNT_ID = ""
+CIN7_APPLICATION_KEY = ""
 
 HEADERS = {
     "Content-type": "application/json",
