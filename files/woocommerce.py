059ee8c08382c177117ec5e7a5b805c1954a7058
Faisal Fida <69955157+faisal-fida@users.noreply.github.com>
2025-05-29 13:51:44 +0500

    Update woocommerce.py

--- files/woocommerce.py
+++ files/woocommerce.py
@@ -1,8 +1,8 @@
 import requests
 from requests.auth import HTTPBasicAuth
 
-consumer_key = "ck_2aa5a27ace2c00b3ca3878b26782c4348bfe10c1"
-consumer_secret = "cs_78ab0cd475b274782f1f6f76fa90bd134417ff75"
+consumer_key = ""
+consumer_secret = ""
 
 url = "https://computia.me/wp-json/wc/v3/orders"
 
